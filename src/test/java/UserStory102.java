6d071af0a92f9843e8de31ae63de5067c439688f
Dwolday <danaittewolde@yahoo.com>
2021-07-01 16:38:38 -0500

    User story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,2 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class UserStory102 {
+    public static void main(String[] args) {
+        System.out.println("hell2oidhfosdjh");
+    }
 }
